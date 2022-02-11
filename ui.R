1ebf91018fe5cadfa984c14488ef61aa0c76e461
Sam McFarland <smcfarl75@gmail.com>
2022-02-11 16:46:41 -0500

    Chart and data was added to Shiny

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Group 14"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
