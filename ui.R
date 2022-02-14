2c7d4b9495085b6f6c4045796b62b1a8c4e3bc3e
Stephen Monroe <64500708+smonroe6@users.noreply.github.com>
2022-02-13 20:50:26 -0500

    Clean up ui.R to remove template code and comments

--- ui.R
+++ ui.R
@@ -9,27 +9,19 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
     titlePanel("Group 14"),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
             selectInput(inputId = "Purpose", 
                         label = "Purpose", 
                         choices = list("Business", "Holiday", "Visiting", "Other"), 
                         selected = 1)
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
             plotOutput("distPlot")
         )
