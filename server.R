eb17619746edaa4110e2b7646f1818d5703f35a6
Stephen Monroe <64500708+smonroe6@users.noreply.github.com>
2022-02-13 20:52:21 -0500

    Clean up server.R to remove template code and comments
    Also added purpose selection to autoplot function

--- server.R
+++ server.R
@@ -11,21 +11,11 @@ library(shiny)
 library(fpp3)
 data("tourism")
 
-
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$distPlot <- renderPlot({
-        t <- tourism %>%filter(Region %in% c("Outback"))
+        t <- tourism %>%filter(Region %in% c("Outback") & Purpose %in% c(input$Purpose))
         autoplot(t)
-        # generate bins based on input$bins from ui.R
-       # x    <- faithful[, 2]
-    #    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-     #   hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
     })
     
 })
